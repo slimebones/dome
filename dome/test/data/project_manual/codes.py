777276d60ab55f543f53e34564ff21cd5b98c058
ryzhovalex <thed4rkof@gmail.com>
2026-04-16 09:09:46 +0300

    Update build.py, code.txt, codes.py

--- dome/test/data/project_manual/codes.py
+++ dome/test/data/project_manual/codes.py
@@ -1,10 +1,12 @@
 ok = 0
 error = 1
-check_error = 2
-validation_error = 3
+check = 2
+validation = 3
+not_found = 4
 
 codenames: dict[int, str] = {
     1: "error",
-    2: "check_error",
-    3: "validation_error",
+    2: "check",
+    3: "validation",
+    4: "not_found",
 }
